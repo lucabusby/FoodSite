cbf494e056a49d68d571469092ecbf3a1fe0b756
lucabusby <128382812+lucabusby@users.noreply.github.com>
2023-08-01 11:58:44 +0200

    recipie lightbox

--- scripts.js
+++ scripts.js
@@ -4,25 +4,77 @@ const recipes = [
       difficulty: "easy",
       food: "meat",
       base: "pasta",
-      origin: "Europe Italy",
+      origin: "Italy, Europe",
+      link: "https://lucabusby.github.io/FoodSite/",
     },
     {
       name: "Veggie Stir-Fry",
       difficulty: "medium",
       food: "vegetarian",
       base: "none",
-      origin: "Asia China",
+      origin: "China, Asia",
+      link: "https://lucabusby.github.io/FoodSite/",
     },
     {
       name: "Boeuf Bourguignon",
       difficulty: "hard",
       food: "meat",
       base: "none",
-      origin: "Europe France",
+      origin: "France, Europe",
+      link: "https://lucabusby.github.io/FoodSite/",
     },
     // Add more recipes here
   ];
   
+  function createRecipeOverlay(recipe) {
+    const overlay = document.createElement("div");
+    overlay.classList.add("overlay");
+  
+    const content = document.createElement("div");
+    content.classList.add("overlay-content");
+  
+    const closeBtn = document.createElement("span");
+    closeBtn.textContent = "×";
+    closeBtn.classList.add("close-btn");
+    closeBtn.addEventListener("click", () => {
+      overlay.remove();
+    });
+  
+    const recipeDetails = document.createElement("div");
+    recipeDetails.classList.add("recipe-details");
+  
+    const recipeName = document.createElement("h2");
+    recipeName.textContent = recipe.name;
+  
+    const recipeDifficulty = document.createElement("p");
+    recipeDifficulty.textContent = "Difficulty: " + recipe.difficulty;
+
+    const recipeFood = document.createElement("p");
+    recipeFood.textContent = "Food: " + recipe.food;
+
+    const recipeBase = document.createElement("p");
+    recipeBase.textContent = "Base: " + recipe.base;
+  
+    const recipeOrigin = document.createElement("p");
+    recipeOrigin.textContent = "Origin: " + recipe.origin;
+
+    const recipeLink = document.createElement("p");
+    recipeLink.textContent = "Link: " + recipe.link;
+  
+    recipeDetails.appendChild(recipeName);
+    recipeDetails.appendChild(recipeDifficulty);
+    recipeDetails.appendChild(recipeFood);
+    recipeDetails.appendChild(recipeBase);
+    recipeDetails.appendChild(recipeOrigin);
+    recipeDetails.appendChild(recipeLink);
+  
+    content.appendChild(closeBtn);
+    content.appendChild(recipeDetails);
+    overlay.appendChild(content);
+  
+    document.body.appendChild(overlay);
+  }
+
   function displayRecipes() {
     const recipesContainer = document.getElementById("recipes-container");
     recipesContainer.innerHTML = "";
@@ -41,8 +93,12 @@ const recipes = [
         (!originFilter || recipe.origin.toLowerCase().includes(originFilter)) &&
         (!searchFilter || recipe.name.toLowerCase().includes(searchFilter))
       ) {
+        
         const recipeCard = document.createElement("div");
         recipeCard.classList.add("recipe-card");
+        recipeCard.addEventListener("click", () => {
+          createRecipeOverlay(recipe);
+        });
   
         const imagePlaceholder = document.createElement("img");
         imagePlaceholder.alt = "Recipe Image";
