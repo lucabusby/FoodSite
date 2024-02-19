47b24dc64e96868e58c517173db964fa580decb5
lucabusby <128382812+lucabusby@users.noreply.github.com>
2024-02-19 19:48:49 +0100

    recipe update and column responsive

--- recipes.js
+++ recipes.js
@@ -25,7 +25,7 @@ const recipes = [
     "food": "vegetarian",
     "base": "none",
     "origin": "Switzerland, Europe",
-    "imageLink": "https://wildeisen-prod-fastly.yanova.ch/assets/image/8e4f6cb2-a0e6-42ee-9f65-9aeb2a0ef231",
+    "imageLink": "https://www.bio-suisse.ch/.imaging/mte/biosuisse-theme/medium/dam/konsumenten/rezepte/Bilder/BIO_BIOSUISSE_072012_04.jpg/jcr:content/BIO_BIOSUISSE_072012_04.jpg",
     "link": "https://www.wildeisen.ch/rezepte/kaese-capuns-mit-kraeutern",
     "notes": ""
   },
@@ -48,16 +48,6 @@ const recipes = [
     "imageLink": "https://klaraslife.com/wp-content/uploads/2019/01/taboulehDSC09023.jpg",
     "link": "",
     "notes": "Couscous\n\nMix with cucumber, cherry tomatoes, paprika, peppermint, raisins, feta cheese, pomegranate &/ figs"
-  },
-  {
-    "name": "Mauazna",
-    "difficulty": "hard",
-    "food": "vegetarian",
-    "base": "bread",
-    "origin": "France, Europe",
-    "imageLink": "https://crumbsandcaramel.com/wp-content/uploads/2018/06/IMG_3522-2-300x300.jpg",
-    "link": "",
-    "notes": "verschideni gmües arte (kein broccoli), mind 3h choche (mit saitan)"
   },
   {
     "name": "Mushroom Leek Pasta",
@@ -210,13 +200,143 @@ const recipes = [
       "notes": ""
     },
     {
-      "name": "Rice Noodle Salad",
+      "name": "Onigiri",
+      "difficulty": "easy",
+      "food": "vegetarian",
+      "base": "rice",
+      "origin": "Japan, Asia",
+      "imageLink": "https://www.eat-this.org/wp-content/uploads/2023/05/eat_this_vegane_onigiri_mit_kichererbsen-thunfisch_016.jpg",
+      "link": "https://www.youtube.com/watch?v=t9zv-_5aOoo&ab_channel=YukainTokyo",
+      "notes": ""
+    },
+    {
+      "name": "Thai Red Curry",
       "difficulty": "medium",
-      "food": "vegan",
-      "base": "noodles",
+      "food": "vegetarian",
+      "base": "rice",
+      "origin": "Thailand, Asia",
+      "imageLink": "https://eadn-wc05-5516459.nxedge.io/wp-content/uploads/2022/10/Thai-Red-Curry-by-Raya-70-Full-plate-square.jpg",
+      "link": "https://www.thaicookbook.tv/thai-recipes/main-dishes/red-curry-with-bamboo-shoots-chicken-and-coconut/",
+      "notes": ""
+    },
+    {
+      "name": "Tikka Masala",
+      "difficulty": "easy",
+      "food": "meat",
+      "base": "none",
+      "origin": "India, Asia",
+      "imageLink": "https://hips.hearstapps.com/hmg-prod/images/chicken-tikka-masala1-1663341991.jpg?crop=0.683xw:1.00xh;0.221xw,0&resize=1200:*",
+      "link": "",
+      "notes": ""
+    },
+    {
+      "name": "Butter Chicken",
+      "difficulty": "medium",
+      "food": "meat",
+      "base": "bread",
+      "origin": "India, Asia",
+      "imageLink": "https://www.cookingclassy.com/wp-content/uploads/2021/01/butter-chicken-4.jpg",
+      "link": "",
+      "notes": "no recipe yet, please send a good one"
+    },
+    {
+      "name": "Bao Buns",
+      "difficulty": "easy",
+      "food": "vegetarian",
+      "base": "bread",
       "origin": "China, Asia",
-      "imageLink": "https://www.recipetineats.com/wp-content/uploads/2014/08/Vermicelli-Noodle-Salad_7.jpg?resize=650,910",
-      "link": "https://www.recipetineats.com/asian-vermicelli-noodle-salad/",
+      "imageLink": "https://www.kitchensanctuary.com/wp-content/uploads/2019/07/Gua-Bao-Buns-Pork-Belly-tall-FS-7272.webp",
+      "link": "https://redhousespice.com/bao/",
+      "notes": ""
+    },
+    {
+      "name": "Summer Rolls",
+      "difficulty": "easy",
+      "food": "vegetarian",
+      "base": "rice",
+      "origin": "Vietnam, Asia",
+      "imageLink": "https://usercontent.one/wp/veggieanh.com/wp-content/uploads/2023/05/Vegan-Summer-Rolls-6.jpg?media=1701510560",
+      "link": "https://veggieanh.com/vegan-summer-rolls/",
+      "notes": ""
+    },
+    {
+      "name": "Sushi",
+      "difficulty": "easy",
+      "food": "vegetarian",
+      "base": "rice",
+      "origin": "Japan, Asia",
+      "imageLink": "https://www.tastingtable.com/img/gallery/the-ingredient-traditional-sushi-never-uses/l-intro-1663275947.jpg",
+      "link": "https://www.youtube.com/watch?v=nIoOv6lWYnk&t=635s&ab_channel=Epicurious",
+      "notes": ""
+    },
+    {
+      "name": "Korean Fried Chicken",
+      "difficulty": "easy",
+      "food": "meat",
+      "base": "none",
+      "origin": "South Korea, Asia",
+      "imageLink": "https://www.kitchensanctuary.com/wp-content/uploads/2019/08/Korean-Fried-Chicken-foodporn-7373.jpg",
+      "link": "https://mykoreankitchen.com/korean-fried-chicken/",
+      "notes": ""
+    },
+    {
+      "name": "Taco",
+      "difficulty": "easy",
+      "food": "vegetarian",
+      "base": "bread",
+      "origin": "Mexico, North America",
+      "imageLink": "https://www.aheadofthyme.com/wp-content/uploads/2021/03/mexican-ground-beef-tacos-6.jpg",
+      "link": "https://dayofthedead.holiday/recipes/5-amazing-mexican-taco-recipes-and-origins/",
+      "notes": ""
+    },
+    {
+      "name": "Fajitas",
+      "difficulty": "easy",
+      "food": "vegetarian",
+      "base": "bread",
+      "origin": "United States, North America",
+      "imageLink": "https://burnhard-springlane-gmbh.stream.prepr.io/w_2304,format_jpg,q_70/https://burnhard-springlane-gmbh.stream.prepr.io/w_1280/qetfpda692c-5hvd9n98ovr3-steak-fajita-vom-skirt-05-titelbild.jpg",
+      "link": "",
+      "notes": "Just put in whatever you want dickhead"
+    },
+    {
+      "name": "Glass Noodle Salad",
+      "difficulty": "easy",
+      "food": "vegetarian",
+      "base": "rice",
+      "origin": "Thailand, Asia",
+      "imageLink": "https://www.venturists.net/wp-content/uploads/2022/04/glass-noodle-salad-720x720.jpeg",
+      "link": "",
+      "notes": "•⁠ ⁠100g glasnudeln (mit kochendem wasser einweichen, mit schere verkleidern)\n• 250g hackfleisch (würzen, braten)\n• 20 cherrytomaten, 2 schallotten, 3 frühlingszwiebeln, 2 knoblauchzehen, chili, evtl. karotten, sellerie oder was sonst noch so rumliegt (klein schneiden)\n• dressing: limettensaft, fischsauce, palmzucker, wenig bouillon \n• anrichten mit koriandergrün und erdnüssen"
+    },
+    {
+      "name": "Gyros",
+      "difficulty": "easy",
+      "food": "vegetarian",
+      "base": "none",
+      "origin": "Greece, Europe",
+      "imageLink": "https://www.greekboston.com/wp-content/uploads/2018/07/Traditional-Gyro.jpg",
+      "link": "https://realgreekrecipes.com/gyros-recipe/",
+      "notes": "replace meat with haloumi"
+    },
+    {
+      "name": "Porcini Risotto",
+      "difficulty": "easy",
+      "food": "vegetarian",
+      "base": "rice",
+      "origin": "Italy, Europe",
+      "imageLink": "https://playswellwithbutter.com/wp-content/uploads/2021/01/Mushroom-Risotto-11.jpg",
+      "link": "https://www.michellemcglinn.com/recipes/porcini-risotto",
+      "notes": ""
+    },
+    {
+      "name": "Green Papaya Salad",
+      "difficulty": "easy",
+      "food": "vegetarian",
+      "base": "vegetables",
+      "origin": "Thailand, Asia",
+      "imageLink": "https://www.recipetineats.com/wp-content/uploads/2021/08/Green-Papaya-Salad_54-SQ.jpg",
+      "link": "https://www.youtube.com/watch?v=n761vBlhn7g&ab_channel=YeungManCooking",
       "notes": ""
     },
   // Add more recipes here
